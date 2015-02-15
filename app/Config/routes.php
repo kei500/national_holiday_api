fbeefdac2b0d942975f2addf4396b307d9bf073d
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2015-02-15 09:02:38 +0000

    add API find all national holidays in specified year

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,4 +1,6 @@
 <?php
 Router::parseExtensions('json');
+
+Router::connect('/years/:year', array('controller' => 'national_holidays', 'action' => 'year'));
 Router::connect('/',      array('controller' => 'national_holidays', 'action' => 'view'));
 Router::connect('/:date', array('controller' => 'national_holidays', 'action' => 'view'));
--- app/Controller/NationalHolidaysController.php
+++ app/Controller/NationalHolidaysController.php
@@ -18,5 +18,36 @@ class NationalHolidaysController extends AppController {
 		$this->set(compact('result'));
 		$this->set('_serialize', 'result');
 	}
+
+	public function year() {
+		$this->viewClass = 'Json';
+
+		$year = $this->params['year'];
+		if (!$year) {
+			throw new NotFoundException;
+		}
+
+		if (!preg_match("/20[0-9]{2,2}/", $year)) {
+			throw new NotFoundException;
+		}
+
+		$conditions = array(
+			'NationalHoliday.date >=' => sprintf("%d-01-01", $year),
+			'NationalHoliday.date <'  => sprintf("%d-01-01", $year + 1),
+		);
+		$national_holidays = $this->NationalHoliday->find('all', array('conditions' => $conditions));
+
+		if (!$national_holidays) {
+			throw new NotFoundException;
+		}
+
+		$result = array();
+		foreach ($national_holidays as $national_holiday) {
+			$result[$national_holiday['NationalHoliday']['date']] = $national_holiday['NationalHoliday']['name'];
+		}
+
+		$this->set(compact('result'));
+		$this->set('_serialize', 'result');
+	}
 }
 ?>
